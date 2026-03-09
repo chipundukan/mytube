e7b20b88d2bfb4955c0d83619efa8850f7e48db2
chipundukan <chipundukan@gmail.com>
2026-03-09 18:23:12 +0200

    Update upload.js

--- video list/upload.js
+++ video list/upload.js
@@ -1,1 +1,15 @@
+function uploadVideo(){
+let title = document.getElementById("title").value;
+let channel = document.getElementById("channel").value;
+let views = Number(document.getElementById("views").value);
+let thumbnail = document.getElementById("thumbnail").value;
+let link = document.getElementById("link").value;
 
+if(title && channel && views && thumbnail && link){
+videos.push({title, channel, views, time:"Just now", thumbnail, link, likes:0, comments:[]});
+alert("Video uploaded!");
+window.location.href = "index.html";
+}else{
+alert("Please fill all fields!");
+}
+}
