f85f6f08f1e58b9647e1d8168f44a6f141ef312c
chipundukan <chipundukan@gmail.com>
2026-03-09 18:29:18 +0200

    Create data.js

---
+++ comments/data.js
@@ -0,0 +1,24 @@
+// data.js
+
+let videos = [
+  {
+    title: "Amazing Football Skills",
+    channel: "Football Pro",
+    views: 2000000,
+    time: "2 days ago",
+    thumbnail: "https://img.youtube.com/vi/1w8Z0UOXVaY/0.jpg",
+    link: "https://www.youtube.com/embed/1w8Z0UOXVaY",
+    likes: 1200,
+    comments: []
+  },
+  {
+    title: "Beautiful Nature",
+    channel: "Nature World",
+    views: 1200000,
+    time: "1 week ago",
+    thumbnail: "https://img.youtube.com/vi/6lt2JfJdGSY/0.jpg",
+    link: "https://www.youtube.com/embed/6lt2JfJdGSY",
+    likes: 850,
+    comments: []
+  }
+];
